edd514b69a6970db6a3f792f75deff6831789988
Rafael J Silva <90015472+RafaSmith2002@users.noreply.github.com>
2022-11-15 19:18:21 -0300

    Meu primeiro React

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Turma React 1 - aula 1
         </p>
         <a
           className="App-link"
